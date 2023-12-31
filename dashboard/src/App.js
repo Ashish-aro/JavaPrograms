2330537c33885b509d41b1048dd4df0ab0801fa5
Ashish-aro <74814394+Ashish-aro@users.noreply.github.com>
2023-12-31 05:02:21 +0000

    React Project

---
+++ dashboard/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './compnents/Navbar';
+import HomePage from './pages/homePage/HomePage';
+
+function App() {
+  return (
+  <>
+  <Navbar />
+  <HomePage />
+  </>
+  );
+}
+
+export default App;
